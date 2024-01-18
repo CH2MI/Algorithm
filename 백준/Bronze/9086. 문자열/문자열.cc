e0192db20cf893f1d4ef153c3da3a821e4ca62e7
CH2MI <47942271+CH2MI@users.noreply.github.com>
2024-01-19 01:57:36 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		string s;
+		cin >> s;
+		cout << s[0] << s[s.size() - 1] << endl;
+	}
+}
