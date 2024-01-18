77a056e9f47edf7c2984e92a45670a290e88f29f
CH2MI <47942271+gwanwoopa@users.noreply.github.com>
2024-01-19 01:48:05 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 208 ms, Memory: 22252 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	int N;
+	cin >> N;
+	int* arr = new int[N + 1]();
+	int* dp = new int[N + 1]();
+	vector<int> v; v.push_back(-1000000001);
+	for (int i = 1; i <= N; i++)
+		cin >> arr[i];
+	dp[0] = -1000000001;
+	int m = 0;
+	int t = 0;
+	for (int i = 1; i <= N; i++) {
+		int cnt = 0;
+		int j;
+		int t;
+		if (v.back() < arr[i]) {
+			v.push_back(arr[i]);
+			t = v.size() - 1;
+		}
+		else {
+			t = lower_bound(v.begin(), v.end(), arr[i]) - v.begin();
+			if (v[t] > arr[i]) v[t] = arr[i];
+		}
+		dp[i] = t;
+		m = max(t, m);
+	}
+	cout << m << '\n';
+	int t1 = m;
+	vector<int> v1;
+	for (int i = N; i >= 1; i--) {
+		if (dp[i] == t1) {
+			v1.push_back(arr[i]);
+			t1--;
+		}
+	}
+	for (auto itr = v1.rbegin(); itr != v1.rend(); itr++) cout << *itr << ' ';
+}
