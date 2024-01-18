1602a7cd5c7d100cb6f971fb70edf32b9661d9bf
CH2MI <47942271+gwanwoopa@users.noreply.github.com>
2024-01-19 01:33:28 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+	int* a = new int[N];
+	for (int i = 0; i < N; i++) {
+		cin >> a[i];
+	}
+
+	int sum = 0;
+	for (int i = 0; i < N - 2; i++) {
+		for (int j = i + 1; j < N - 1; j++) {
+			for (int k = j + 1; k < N; k++) {
+				int s = a[i] + a[j] + a[k];
+				if (sum < s && s <= M) sum = s;
+			}
+		}
+	}
+	cout << sum;
+	delete[] a;
+}
