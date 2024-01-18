be5235140e666eca64f76ad47cb8d1a7c30a4621
CH2MI <47942271+CH2MI@users.noreply.github.com>
2024-01-19 01:52:09 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+	if (n == 0) return 1;
+	return n * factorial(n - 1);
+}
+
+int main() {
+	int N, K;
+	cin >> N >> K;
+	cout << factorial(N) / (factorial(K) * factorial(N - K));
+}
