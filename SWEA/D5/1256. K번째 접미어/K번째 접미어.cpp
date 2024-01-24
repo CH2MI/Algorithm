4e4f8b516777d2a57ed16403839c027d3a6541be
CH2MI <47942271+CH2MI@users.noreply.github.com>
2024-01-24 15:02:58 +0900

    [D5] Title: K번째 접미어, Time: 82 ms, Memory: 42,236 KB -BaekjoonHub

---
+++ SWEA/D5/1256. K번째 접미어/K번째 접미어.cpp
@@ -0,0 +1,96 @@
+#include <cstring>
+#include<iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Trie {
+	static const size_t M = 26;
+	static const char OFFSET = 'a';
+	struct TrieNode {
+		int index;
+		int child[M];
+		bool is_Last;
+		TrieNode() {
+			memset(child, -1, sizeof(int)* M);
+			is_Last = false;
+			index = -1;
+		}
+	};
+	vector<TrieNode> nodes;
+public:
+	Trie() : nodes(1) {}
+	void init() {
+		nodes.resize(1);
+		nodes[0] = TrieNode();
+	}
+	void insert(const string& str, const int& i) {
+		int node_id = 0;
+		for (const auto& c : str) {
+			// 해당 문자열의 자식이 없으면 만들어 준다.
+			if (nodes[node_id].child[c - OFFSET] == -1) {
+				nodes[node_id].child[c - OFFSET] = nodes.size();
+				nodes.emplace_back();
+			}
+			node_id = nodes[node_id].child[c - OFFSET];
+		}
+		// 마지막 부분임을 나타낸다.
+		nodes[node_id].is_Last = true;
+		nodes[node_id].index = i;
+	}
+	void dfs(int index, const int& k, int& count, int& value) {
+		if (nodes[index].is_Last) {
+			count++;
+			if (k == count) {
+				value = nodes[index].index;
+				return;
+			}	
+		}
+		for (int i = 0; i < M; i++) {
+			if (nodes[index].child[i] != -1) {
+				dfs(nodes[index].child[i], k, count, value);
+			}
+			if (k == count) return;
+		}
+	}
+};
+
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	/*
+	   아래의 freopen 함수는 input.txt 를 read only 형식으로 연 후,
+	   앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+	   //여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+	   freopen 함수를 이용하면 이후 cin 을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+	   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 함수를 사용하셔도 좋습니다.
+	   freopen 함수를 사용하기 위해서는 #include <cstdio>, 혹은 #include <stdio.h> 가 필요합니다.
+	   단, 채점을 위해 코드를 제출하실 때에는 반드시 freopen 함수를 지우거나 주석 처리 하셔야 합니다.
+	*/
+	//freopen("input.txt", "r", stdin);
+	cin >> T;
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		Trie trie;
+		trie.init();
+		int N;
+		cin >> N;
+		string s;
+		cin >> s;
+		for (int i = 0; i < s.size(); i++) {
+			string t = s.substr(i);
+			trie.insert(t, i);
+
+		}
+		int count = 0, value = 0;
+		trie.dfs(0, N, count, value);
+		cout << "#" << test_case << ' ';
+		cout << s.substr(value) << '\n';
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
---
+++ SWEA/D5/1256. K번째 접미어/README.md
@@ -0,0 +1,15 @@
+# [D5] K번째 접미어 - 1256 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV18GHd6IskCFAZN) 
+
+### 성능 요약
+
+메모리: 42,236 KB, 시간: 82 ms, 코드길이: 2,418 Bytes
+
+### 제출 일자
+
+2024-01-24 15:02
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
