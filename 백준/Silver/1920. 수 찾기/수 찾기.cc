60063ad30ac91e5f00bcf917dd78c738e8cd07ee
CH2MI <47942271+CH2MI@users.noreply.github.com>
2024-02-26 14:42:11 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <set>
 using namespace std;
 
-void a() {
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
 	int n;
 	cin >> n;
 	vector<int> v(n);
@@ -12,6 +14,7 @@ void a() {
 		cin >> v[i];
 	}
 	sort(v.begin(), v.end());
+
 	int m;
 	cin >> m;
 
@@ -22,31 +25,4 @@ void a() {
 		if (b) cout << "1\n";
 		else cout << "0\n";
 	}
-}
-
-void b() {
-	int n;
-	cin >> n;
-	set<int> s;
-	for (int i = 0; i < n; i++) {
-		int t;
-		cin >> t;
-		s.insert(t);
-	}
-
-	int m;
-	cin >> m;
-
-	for (int i = 0; i < m; i++) {
-		int t;
-		cin >> t;
-		auto b = s.count(t);
-		if (b) cout << "1\n";
-		else cout << "0\n";
-	}
-}
-
-int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL);
-	a();
 }
