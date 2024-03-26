6d8df675068e70bf145d77b6e8298a798ff9f3f1
CH2MI <47942271+CH2MI@users.noreply.github.com>
2024-03-26 15:22:36 +0900

    [Silver II] Title: A → B, Time: 4 ms, Memory: 2532 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.cc
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -6,23 +6,20 @@
 
 	int bfs() {
 		priority_queue<pair<int, long long>> q;
-		vector<bool> visited(B + 1);
-		q.push({ 0, A });
+		q.emplace(0, A);
 		while (!q.empty()) {
 			auto t = q.top();
 			q.pop();
 		
 			if (t.second == B) return -t.first + 1;
 
-			visited[t.second] = 1;
-
 			long long t1 = t.second << 1;
-			if (t1 <= B && !visited[t1])
-				q.push({ t.first - 1, t1 });
+			if (t1 <= B)
+				q.emplace(t.first - 1, t1);
 
 			long long t2 = t.second * 10 + 1;
-			if (t2 <= B && !visited[t2])
-				q.push({ t.first - 1, t2 });
+			if (t2 <= B)
+				q.emplace(t.first - 1, t2);
 		}
 
 		return -1;
