fcb46ce881b1464711091376127b817c5c03fc89
CH2MI <47942271+CH2MI@users.noreply.github.com>
2024-03-26 15:20:49 +0900

    [Silver II] Title: A → B, Time: 80 ms, Memory: 124608 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.cc
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -1,34 +1,37 @@
-#include <iostream>
-#include <queue>
-using namespace std;
+	#include <iostream>
+	#include <queue>
+	using namespace std;
 
-long long A, B;
+	long long A, B;
 
-int bfs() {
-	priority_queue<pair<int, long long>> q;
-	vector<bool> visited(B + 1);
-	q.push({ 0, A });
-	while (!q.empty()) {
-		auto t = q.top();
-		q.pop();
+	int bfs() {
+		priority_queue<pair<int, long long>> q;
+		vector<bool> visited(B + 1);
+		q.push({ 0, A });
+		while (!q.empty()) {
+			auto t = q.top();
+			q.pop();
 		
-		if (t.second > B) continue;
-		if (visited[t.second]) continue;
+			if (t.second == B) return -t.first + 1;
 
-		if (t.second == B) return -t.first + 1;
+			visited[t.second] = 1;
 
-		visited[t.second] = 1;
+			long long t1 = t.second << 1;
+			if (t1 <= B && !visited[t1])
+				q.push({ t.first - 1, t1 });
 
-		q.push({ t.first - 1, t.second * 2 });
-		q.push({ t.first - 1, t.second * 10 + 1 });
+			long long t2 = t.second * 10 + 1;
+			if (t2 <= B && !visited[t2])
+				q.push({ t.first - 1, t2 });
+		}
+
+		return -1;
 	}
-	return -1;
-}
 
-int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	int main() {
+		ios_base::sync_with_stdio(false); cin.tie(NULL);
 	
-	cin >> A >> B;
+		cin >> A >> B;
 
-	cout << bfs();
-}
+		cout << bfs();
+	}
