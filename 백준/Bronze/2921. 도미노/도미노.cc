9e14a11fa05ce88c382a2a6fdc318cbd3737ccde
CH2MI <47942271+CH2MI@users.noreply.github.com>
2024-06-20 19:32:16 +0900

    [Bronze III] Title: 도미노, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2921. 도미노/도미노.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N; cin >> N;
+    int sum = 0;
+    for (int i = 0; i <= N; i++) {
+        for (int j = 0; j <= i; j++) {
+            sum += i + j;
+        }
+    }
+    cout << sum;
+}
