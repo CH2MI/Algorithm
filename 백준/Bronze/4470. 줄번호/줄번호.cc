1edafaa5d8d68e979bb164aa8dd992a25d34d4e8
CH2MI <47942271+CH2MI@users.noreply.github.com>
2024-09-01 14:59:06 +0900

    [Bronze IV] Title: 줄번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    cin.ignore();
+    
+    for (int i = 1; i <= N; i++) {
+        string s;
+        getline(cin, s);
+        cout << i << ". " << s << '\n';
+    }
+}
