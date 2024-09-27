3e1ffe89df3f0f67c5cbd29bd1299b894cca9683
CH2MI <47942271+CH2MI@users.noreply.github.com>
2024-09-27 23:54:08 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 88 ms, Memory: 9384 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int N;
+vector<vector<int>> adj;
+vector<int> visited;
+int cnt = 1;
+
+void BFS(int r) {
+	queue<int> q;
+	q.push(r);
+
+	while (!q.empty()) {
+		auto n = q.front();
+		q.pop();
+
+		if (visited[n]) continue;
+
+		visited[n] = cnt++;
+		sort(adj[n].begin(), adj[n].end(), greater<>());
+
+		for (auto i : adj[n]) {
+			q.push(i);
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int M, R;
+	cin >> N >> M >> R;
+
+	adj.assign(N, vector<int>());
+	visited.assign(N, 0);
+
+	for (int i = 0; i < M; i++) {
+		int s, e;
+		cin >> s >> e;
+		adj[s - 1].push_back(e - 1);
+		adj[e - 1].push_back(s - 1);
+	}
+
+	BFS(R - 1);
+
+	for (auto i : visited) cout << i << '\n';
+
+}
