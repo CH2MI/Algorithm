714b3a6241f84151ddcbe15a243013a9e93498d8
CH2MI <47942271+CH2MI@users.noreply.github.com>
2024-09-27 23:50:01 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 88 ms, Memory: 12492 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N;
+vector<vector<int>> adj;
+vector<int> visited;
+int cnt = 1;
+
+void DFS(int n) {
+	visited[n] = cnt++;
+
+	sort(adj[n].begin(), adj[n].end(), greater<>());
+
+	for (auto i : adj[n]) {
+		if (visited[i]) continue;
+		DFS(i);
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int M, R;
+	cin >> N >> M >> R;
+
+	adj.assign(N, vector<int>());
+	visited.assign(N, 0);
+
+	for (int i = 0; i < M; i++) {
+		int s, e;
+		cin >> s >> e;
+		adj[s - 1].push_back(e - 1);
+		adj[e - 1].push_back(s - 1);
+	}
+
+	DFS(R - 1);
+
+	for (auto i : visited) cout << i << '\n';
+
+}
