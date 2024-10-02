c4f441660dbfe0b33debaebdcc2c130c1d9919bd
CH2MI <47942271+CH2MI@users.noreply.github.com>
2024-10-02 12:37:56 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -9,9 +9,8 @@ int main() {
 	int X; cin >> X;
 
 	vector<int> dp(X + 1);
-	dp[2] = 1; dp[3] = 1;
-	
-	for (int i = 4; i <= X; i++) {
+    
+	for (int i = 2; i <= X; i++) {
 		int mn = dp[i - 1];
 		if (i % 2 == 0) mn = min(mn, dp[i / 2]);
 		if (i % 3 == 0) mn = min(mn, dp[i / 3]);
