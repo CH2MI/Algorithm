79240b64e350649dd6d27902aad2948358137a5f
CH2MI <47942271+CH2MI@users.noreply.github.com>
2024-12-12 12:49:39 +0900

    [Bronze III] Title: 접시 안의 원, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16483. 접시 안의 원/접시 안의 원.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    double T; cin >> T;
+    cout << (int)round((T / 2) * (T / 2));
+}
