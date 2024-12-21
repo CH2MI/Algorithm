70afaa518a47622a2295d52fffe4f433f023e9da
CH2MI <47942271+CH2MI@users.noreply.github.com>
2024-12-21 15:37:40 +0900

    [Bronze II] Title: 단어의 개수, Time: 20 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -1,15 +1,12 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	string s;
-	getline(cin, s);
-	int i = 0, count = 0, t = s.size();
-	if (s[0] == ' ') i++;
-	for (; i < t; i++) {
-		if (s[i] == ' ') count++;
-	}
-	if (s[t - 1] != ' ') count++;
-	cout << count;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    int cnt = 0;
+    while (cin >> s) cnt++;
+    cout << cnt;
 }
