981666de387bdd0ea29a25e04a695f154c957eea
CH2MI <47942271+CH2MI@users.noreply.github.com>
2025-01-09 11:17:41 +0900

    [Platinum V] Title: 볼록볼록, Time: 80 ms, Memory: 6708 KB -BaekjoonHub

--- 백준/Platinum/31236. 볼록볼록/볼록볼록.cc
+++ 백준/Platinum/31236. 볼록볼록/볼록볼록.cc
@@ -4,8 +4,6 @@ using namespace std;
 typedef long long ll;
 typedef array<ll, 2> point;
 
-int N;
-
 int CCW(point& a, point& b, point& c) {
     ll ans = a[0] * b[1] + b[0] * c[1] + c[0] * a[1] - (b[0] * a[1] + c[0] * b[1] + a[0] * c[1]);
     if (ans > 0) return 1;
@@ -13,7 +11,14 @@ int CCW(point& a, point& b, point& c) {
     return 0;
 }
 
-int cal(vector<point>& v) {
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N; cin >> N;
+    vector<point> v(N);
+    for (auto& [x, y] : v) cin >> x >> y;
+
     int mx = -1;
     int l = 0, r = 0;
 
@@ -23,22 +28,9 @@ int cal(vector<point>& v) {
             continue;
         }
 
-        if (CCW(v[r - 2], v[r - 1], v[r]) > 0) {
-            if (CCW(v[r - 1], v[r], v[l]) > 0 && CCW(v[r], v[l], v[l + 1]) > 0) mx = max(mx, r - l + 1), r++;
-            else l++;
-        }
+        if (CCW(v[r - 2], v[r - 1], v[r]) > 0 && CCW(v[r - 1], v[r], v[l]) > 0 && CCW(v[r], v[l], v[l + 1]) > 0) mx = max(mx, r - l + 1), r++;
         else l++;
     }
-    return mx;
-}
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    cin >> N;
-    vector<point> v(N);
-    for (auto& [x, y] : v) cin >> x >> y;
-
-    cout << cal(v);
+    cout << mx;
 }
