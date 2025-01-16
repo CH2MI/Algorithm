38a355ede40fd7d1d4ad1f880fd8bc607053bdc3
CH2MI <47942271+CH2MI@users.noreply.github.com>
2025-01-16 11:09:33 +0900

    [Platinum IV] Title: 가위바위보, Time: 4 ms, Memory: 2656 KB -BaekjoonHub

---
+++ 백준/Platinum/2207. 가위바위보/가위바위보.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, S, cnt;
+vector<int> sn, dt;
+vector<vector<int>> adj;
+stack<int> st;
+
+int DFS(int cur) {
+    int ret = dt[cur] = cnt++;
+    st.push(cur);
+
+    for (int& nxt : adj[cur]) {
+        if (dt[nxt] == -1) ret = min(ret, DFS(nxt));
+        else if (sn[nxt] == -1) ret = min(ret, dt[nxt]);
+    }
+
+    if (ret == dt[cur]) {
+        while (1) {
+            int u = st.top(); st.pop();
+            sn[u] = S;
+            if (u == cur) break;
+        }
+        S++;
+    }
+    return ret;
+}
+
+bool SCC() {
+    S = cnt = 0;
+    sn = dt = vector(n << 1, -1);
+
+    for (int i = 0; i < n << 1; i++) if (dt[i] == -1) DFS(i);
+
+    for (int i = 0; i < n; i++) {
+        if (sn[i << 1] == sn[(i << 1) + 1]) return 0;
+    }
+
+    return 1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int m; cin >> m >> n;
+
+    adj.assign(n << 1, vector<int>());
+
+    for (int i = 0; i < m; i++) {
+        int s, e; cin >> s >> e;
+        int a = s < 0, b = e < 0;
+        adj[(abs(s) - 1 << 1) + !a].push_back((abs(e) - 1 << 1) + b);
+        adj[(abs(e) - 1 << 1) + !b].push_back((abs(s) - 1 << 1) + a);
+    }
+
+    if (SCC()) cout << "^_^";
+    else cout << "OTL";
+}
