38767627f7e8bfba6a524f6e3134201424b1f49f
CH2MI <47942271+CH2MI@users.noreply.github.com>
2025-06-02 11:22:16 +0900

    [Gold V] Title: 더하기와 나누기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/28426. 더하기와 나누기/더하기와 나누기.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    int N; cin >> N;
+    cout << 3 << ' ';
+    for (int i = 1; i < N; i++) cout << i * 6 << ' ';
+
+}
