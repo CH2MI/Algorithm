26d2267c86e9695180db5a1cfae798a6280fabd2
CH2MI <47942271+CH2MI@users.noreply.github.com>
2025-07-09 19:09:32 +0900

    [Silver I] Title: 수라도, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/34043. 수라도/수라도.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    int N; cin >> N;
+    if (N == 1 || N == 3) cout << "NO";
+    else cout << "YES";
+}
