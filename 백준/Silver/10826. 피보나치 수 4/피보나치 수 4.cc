978362e31f540d4d54eaa6bc4f529b2ebab4440e
CH2MI <47942271+CH2MI@users.noreply.github.com>
2025-08-20 21:47:49 +0900

    [Silver V] Title: 피보나치 수 4, Time: 288 ms, Memory: 13040 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.cc
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Integer {
+public:
+    Integer() = default;
+    Integer(string s) {
+        num = s;
+    }
+
+    Integer operator+(const Integer& B) const {
+        string a = num;
+        string b = B.num;
+
+        if (a.size() < b.size()) swap(a, b);
+        reverse(a.begin(), a.end());
+        reverse(b.begin(), b.end());
+
+        int i, t = 0;
+        string ans;
+
+        for (i = 0; i < b.size(); i++) {
+            int x = a[i] - '0';
+            int y = b[i] - '0';
+            string z = to_string(x + y + t);
+            if (z.size() == 2) t = z.front() - '0';
+            else t = 0;
+            ans.append(&z.back());
+        }
+
+        for (; i < a.size(); i++) {
+            int x = a[i] - '0';
+            string z = to_string(x + t);
+            if (z.size() == 2) t = z.front() - '0';
+            else t = 0;
+            ans.append(&z.back());
+        }
+
+        if (t) ans.push_back(static_cast<char>(t + '0'));
+        reverse(ans.begin(), ans.end());
+
+        return Integer(ans);
+    }
+
+    friend ostream& operator<<(ostream& os, const Integer& integer) {
+        os << integer.num;
+        return os;
+    }
+
+private:
+    string num{};
+};
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    Integer dp[10001];
+    dp[0] = Integer("0");
+    dp[1] = Integer("1");
+
+    for (int i = 2; i <= 10000; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    int N; cin >> N;
+    cout << dp[N];
+
+
+}
