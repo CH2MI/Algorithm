aa72225f63706338ab2976d620a1b05ca8769b26
CH2MI <47942271+CH2MI@users.noreply.github.com>
2025-09-27 00:08:09 +0900

    [Platinum V] Title: 뭉쳐야 산다, Time: 592 ms, Memory: 108112 KB -BaekjoonHub

---
+++ 백준/Platinum/28277. 뭉쳐야 산다/뭉쳐야 산다.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    int N, Q; cin >> N >> Q;
+    vector<unordered_set<int>> v(N);
+
+    for (int i = 0; i < N; i++) {
+        int n; cin >> n;
+        for (int j = 0; j < n; j++) {
+            int s; cin >> s;
+            v[i].insert(s);
+        }
+    }
+
+    for (int i = 0; i < Q; i++) {
+        int q; cin >> q;
+        if (q == 1) {
+            int a, b; cin >> a >> b;
+            if (v[a - 1].size() < v[b - 1].size()) swap(v[a - 1], v[b - 1]);
+            for (int j : v[b - 1]) v[a - 1].insert(j);
+            v[b - 1] = unordered_set<int>();
+        }
+        else {
+            int a; cin >> a;
+            cout << v[a - 1].size() << '\n';
+        }
+    }
+}
