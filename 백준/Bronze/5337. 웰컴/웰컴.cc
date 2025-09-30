860b5e1cf0495867b7e3624c43302a034e29f6d8
CH2MI <47942271+CH2MI@users.noreply.github.com>
2025-09-30 23:24:44 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    cout << ".  .   .\n";
+    cout << "|  | _ | _. _ ._ _  _\n";
+    cout << "|/\\|(/.|(_.(_)[ | )(/.";
+}
