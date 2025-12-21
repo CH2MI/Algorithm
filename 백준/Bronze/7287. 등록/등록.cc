020edaf6e6a7ec0571bd0f68c5dbf225d5ae43f4
CH2MI <47942271+CH2MI@users.noreply.github.com>
2025-12-21 23:58:19 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cout << "1227\ngwanwoopa";
+}
