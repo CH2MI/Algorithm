46e8acf78ee111df30ccda3de5ad8fd32ce60d46
CH2MI <47942271+CH2MI@users.noreply.github.com>
2026-03-07 23:28:26 +0900

    [Silver II] Title: K번째 소수, Time: 196 ms, Memory: 46948 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.cc
+++ 백준/Silver/15965. K번째 소수/K번째 소수.cc
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+constexpr int MAX = 10'000'000;
+
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 
@@ -8,16 +10,16 @@ int main() {
     freopen("input.txt", "r", stdin);
 #endif
 
-    vector<int> is_prime(1'000'001, 1);
-    for (int i = 2; i * i <= 1'000'000; i++) {
+    vector<int> is_prime(MAX + 1, 1);
+    for (int i = 2; i * i <= MAX; i++) {
         if (!is_prime[i]) continue;
 
-        for (int j = i + i; j <= 1'000'000; j += i) is_prime[j] = 0;
+        for (int j = i + i; j <= MAX; j += i) is_prime[j] = 0;
     }
 
     vector<int> p; p.reserve(500'000);
 
-    for (int i = 2; i <= 1'000'000; i++) if (is_prime[i]) p.push_back(i);
+    for (int i = 2; i <= MAX; i++) if (is_prime[i]) p.push_back(i);
 
     int K; cin >> K;
     cout << p[K - 1];
