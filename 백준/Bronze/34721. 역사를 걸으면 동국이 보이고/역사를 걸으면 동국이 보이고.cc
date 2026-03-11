6a5b8ac6f0d3c10b3585d1f298a6c7c2c0482bbc
CH2MI <47942271+CH2MI@users.noreply.github.com>
2026-03-11 20:33:06 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    int N; cin >> N;
+    for (int i = 0; i < N; i++) {
+        cout << "I love DGU\n";
+    }
+}
