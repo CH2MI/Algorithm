02f08fd3798e2765f8d0c94035a472baacb2220d
CH2MI <47942271+CH2MI@users.noreply.github.com>
2026-03-23 23:14:42 +0900

    [Bronze IV] Title: 히스토그램, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    int T; cin >> T;
+    while (T--) {
+        int N; cin >> N;
+        while (N--) cout << "=";
+        cout <<'\n';
+    }
+}
